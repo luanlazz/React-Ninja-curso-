53ac28d1ab4a4b499b9eafd69b788f83284d8fd1
luan.lazzari <luanlazzari@gmail.com>
2020-01-25 17:03:01 -0300

    Manipulate DOM

---
+++ modulo1-3rd-party-with-react/src/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React, { Component } from 'react'
+import Plugin from './plugin'
+
+class App extends Component {
+  render () {
+    return (
+      <Plugin />
+    )
+  }
+}
+
+export default App
