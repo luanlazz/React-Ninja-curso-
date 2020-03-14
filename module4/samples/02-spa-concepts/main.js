25bbf4794a90985d8b96f7956e3b1ace156a83fc
luan.lazzari <luanlazzari@gmail.com>
2020-03-14 17:39:49 -0300

    spa concepts

---
+++ module4/samples/02-spa-concepts/main.js
@@ -0,0 +1,36 @@
+;(function () {
+  'use strict'
+
+  var $links = document.querySelectorAll('a')
+  var $h1 = document.querySelector('h1')
+
+  $links.forEach(function ($link) {
+    $link.addEventListener('click', handleClick, false)
+  })
+
+  window.onpopstate = function (e) {
+    navigation({ url: e.state.url })
+  }
+
+  history.replaceState({ url: '/' }, '', '/')
+
+  function navigation (state) {
+    var titles = {
+      '/': 'Home',
+      '/sobre': 'Sobre'
+    }
+    $h1.textContent = titles[state.url]
+  }
+
+  function handleClick (e) {
+    e.preventDefault()
+    var url = e.target.getAttribute('href')
+    var state = {
+      url: url
+    }
+    var title = ''
+    history.pushState(state, title, url)
+    navigation({ url: url })
+  }
+
+})()
